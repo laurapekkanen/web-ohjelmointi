424971443861c1b9ad3890859c8181874827f3ea
Laura Pekkanen <K8966@LABRANET.JAMK.FI>
2017-11-06 13:26:15 +0200

    edit js

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
-var Message = React.createClass({
+/*var Weather = React.createClass({
 
-   /* getWeather: function(){
+    getWeather: function(){
         $.ajax({
             url: "http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=de1abf67dd420d2455bc69e2ceb2a139",
             cache: false,
@@ -18,47 +18,58 @@ var Message = React.createClass({
         $.each(data.weather, function(index, weather) {
             $("#saa").append("<li>"+weather.id+" " +weather.main+ " " + weather.description+"</li>");
         });
-    }*/
+    }
     
    render: function() {
                 return (
                     <p>{this.props.message}</p>
                 );
             }
-        });
+});*/
 
     
 
-    // create Messages component
-        var Messages = React.createClass({
-            render: function() {
-                return (
-                    <div>
-                        <form>
-                            <input placeholder="Valitse paikkakunta"/>
-                            <button>Hae</button><br/>
-                            <button>Ma</button>
-                            <button>Ti</button>
-                            <button>Ke</button>
-                            <button>To</button>
-                            <button>Pe</button>
-                            <button>La</button>
-                            <button>Su</button>
-                            <section id="saa">
-                                tänne sää tiedot?
-                            </section>
-                        </form>
-                    </div>
-                );
-            }
+
+var Weather = React.createClass({
+    getWeather: function(){
+        $.ajax({
+            url: "http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=de1abf67dd420d2455bc69e2ceb2a139",
+            cache: false,
+            dataType: 'json',
+            success: function(data) {
+                this.showWeather({datas: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.log('VIRHE: ', status, err.toString());
+            }.bind(this),
         });
+    },
 
-
-
-
+    
+    render: function() {
+        return (
+            <div>
+                <form>
+                    <input placeholder="Valitse paikkakunta"/>
+                    <button>Hae</button><br/>
+                    <button>Ma</button>
+                    <button>Ti</button>
+                    <button>Ke</button>
+                    <button>To</button>
+                    <button>Pe</button>
+                    <button>La</button>
+                    <button>Su</button>
+                    <section id="saa">
+                        tänne sää tiedot?
+                    </section>
+                </form>
+            </div>
+        );
+    }
+});
 
 
 ReactDOM.render(
-    <Messages />, 
+    <Weather />, 
     document.getElementById("root")
 );
