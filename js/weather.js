51131a1e275d9bb0d835d4f366093a85ff92d4a6
laurahuotari <k8237@LABRANET.JAMK.FI>
2017-11-06 13:20:29 +0200

    edit js

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,12 @@
-var Weather = React.createClass({
+var Message = React.createClass({
 
-    getWeather: function(){
+   /* getWeather: function(){
         $.ajax({
             url: "http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=de1abf67dd420d2455bc69e2ceb2a139",
             cache: false,
             dataType: 'json',
             success: function(data) {
-                this.setState({datas: data});
+                this.showWeather({datas: data});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.log('VIRHE: ', status, err.toString());
@@ -14,15 +14,51 @@ var Weather = React.createClass({
         });
     },
 
-    function showWeather(data) {
+    showWeather(data) {
         $.each(data.weather, function(index, weather) {
             $("#saa").append("<li>"+weather.id+" " +weather.main+ " " + weather.description+"</li>");
         });
-    }
+    }*/
+    
+   render: function() {
+                return (
+                    <p>{this.props.message}</p>
+                );
+            }
+        });
 
-});
+    
+
+    // create Messages component
+        var Messages = React.createClass({
+            render: function() {
+                return (
+                    <div>
+                        <form>
+                            <input placeholder="Valitse paikkakunta"/>
+                            <button>Hae</button><br/>
+                            <button>Ma</button>
+                            <button>Ti</button>
+                            <button>Ke</button>
+                            <button>To</button>
+                            <button>Pe</button>
+                            <button>La</button>
+                            <button>Su</button>
+                            <section id="saa">
+                                tänne sää tiedot?
+                            </section>
+                        </form>
+                    </div>
+                );
+            }
+        });
+
+
+
+
+
 
 ReactDOM.render(
-    <Weather />, 
+    <Messages />, 
     document.getElementById("root")
 );
