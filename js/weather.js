380b41f8dc918208b4463ae1e8f936788007e411
laurapekkanen <k8966@student.jamk.fi>
2017-11-17 13:35:26 +0200

    edit js -> render

--- js/weather.js
+++ js/weather.js
@@ -6,8 +6,6 @@ var Weather = React.createClass({
         return {
             loading: true,
             error: null,
-            mja: 'Arska',
-            datas: null
         };
     },
     
@@ -20,9 +18,8 @@ var Weather = React.createClass({
             dataType: 'json',
             success: function(data) {
                 this.setState({datas: data});
-                this.setState({mja: 'Laura'});
                 console.log('JSON loaded');
-                console.log(data);
+                //console.log(data);
                 //console.log(data.coord.lon);
             }.bind(this),
             error: function(xhr, status, err) {
@@ -38,27 +35,46 @@ var Weather = React.createClass({
     
     //piirrä nämä
     render: function() {
-        //var data = this.props.data;
-        //console.log(data);
+        //temperature
+        var Temp;
+        if (this.state.datas !== undefined) {
+          Temp = <p>{this.state.datas.main.temp}</p> //celcius = -272.15
+        } else {
+          Temp = null;
+        }
+        //humidity
+        var Humi;
+        if (this.state.datas !== undefined) {
+          Humi = <p>{this.state.datas.main.humidity}</p>
+        } else {
+          Humi = null;
+        }
+
         return (
             <div>
                 <form>
                     <input placeholder="Valitse paikkakunta"/>
                     <button>Hae</button><br/>
-                    <button >{this.state.datas} </button> 
-                    <button>Ti</button>
-                    <button>Ke</button>
-                    <button>To</button>
-                    <button>Pe</button>
-                    <button>La</button>
-                    <button>Su</button>
+                    <button >Ma {(new Date().getDate())}.{(new Date().getMonth()+1)}</button> 
+                    <button>Ti{(new Date().getDate()+1)}.{(new Date().getMonth()+1)}</button>
+                    <button>Ke{(new Date().getDate()+2)}.{(new Date().getMonth()+1)}</button>
+                    <button>To{(new Date().getDate()+3)}.{(new Date().getMonth()+1)}</button>
+                    <button>Pe{(new Date().getDate()+4)}.{(new Date().getMonth()+1)}</button>
+                    <button>La{(new Date().getDate()+5)}.{(new Date().getMonth()+1)}</button>
+                    <button>Su{(new Date().getDate()+6)}.{(new Date().getMonth()+1)}</button>
                     <section id="saa">
-                        tänne sää tiedot?
+                        <p>PVM</p>
+                        {(new Date().getDate())}.{(new Date().getMonth()+1)}.{new Date().getFullYear()}
+                        <p>Lämpötila</p>
+                        {Temp}
+                        <p>Kosteus</p>
+                        {Humi}
                     </section>
                 </form>
             </div>
         );
     }
+
 });
 
 ReactDOM.render(
