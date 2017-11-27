7ed6a2f197a0c399f0df5478897595c8e19e28b4
laurahuotari <k8237@LABRANET.JAMK.FI>
2017-11-27 13:00:01 +0200

    edit json

--- js/weatherjquery.js
+++ js/weatherjquery.js
@@ -2,12 +2,10 @@ $(document).ready(function(){
     $('#citybutton').click(function(){
         var city = $('#city').val();
         console.log(city);
-    
-   // var url = "http://api.openweathermap.org/data/2.5/weather?APPID=de1abf67dd420d2455bc69e2ceb2a139&q=" + city +"&units=metric&cnt=7&lang=fi";
+        
         var url ="http://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=metric&lang=en&APPID=de1abf67dd420d2455bc69e2ceb2a139"
         $.getJSON(url, function(result){
             console.log(result);
-
              $('#saa').append('<p>'+result.list[0+7].dt_txt+'</p>');
             $('#saa').append('<p>Lämpötila '+result.list[0].main.temp+'°C</p>');
              $('#saa').append('<p>Kosteus '+result.list[0].main.humidity+'%</p>');
@@ -15,7 +13,7 @@ $(document).ready(function(){
          
         });
     });
-    var lang = moment('2017-03');
+   var lang = moment('2017-03');
     moment.locale('fi');    
     console.log(lang.format('MMMM'));
     var filang = moment('2017-03');
@@ -27,7 +25,5 @@ $(document).ready(function(){
     $('#kolmas').append(moment().add(2, 'days').format('dddd'));
     $('#neljas').append(moment().add(3, 'days').format('dddd'));
     $('#viides').append(moment().add(4, 'days').format('dddd'));
-    
-    
     
 });
